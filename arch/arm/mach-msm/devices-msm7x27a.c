b54963ac4d7f382b5d4cafb8d3323e521e04269c
jonypx09 <jonathan.pelastine@gmail.com>
2014-02-24 17:43:36 -0500

    Update devices-msm7x27a.c

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -679,7 +679,7 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 266760000,
+			.gpu_freq = 269760000,
 			.bus_freq = 200000000,
 		},
 		{
